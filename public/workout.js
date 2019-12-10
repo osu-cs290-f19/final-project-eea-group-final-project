19c9c142ad2908cf55ef444441070912fb956a3b
hoanger9 <55899365+hoanger9@users.noreply.github.com>
2019-12-09 21:15:48 -0800

    filter works, and added workouts css to main css and changed the the main div classes.

--- public/workout.js
+++ public/workout.js
@@ -10,7 +10,7 @@ var legsBtn = document.getElementById("legs");
 var quadsBtn = document.getElementById("quads");
 var hamstringsBtn = document.getElementById("hamstrings");
 var calfsBtn = document.getElementById("calfs");
-filterSelection("all");
+//filterSelection("all");
 
 allBtn.addEventListener("click", filterMovement);
 chestBtn.addEventListener("click", filterMovement);
@@ -22,50 +22,25 @@ quadsBtn.addEventListener("click", filterMovement);
 hamstringsBtn.addEventListener("click", filterMovement);
 calfsBtn.addEventListener("click", filterMovement);
 
-
-
 function filterMovement(){
   var current = document.getElementsByClassName("active");
-  var movement =this.value;
+  var movement = this.value;
+  if (movement === "all") {
+    movement = "";
+  }
   filterSelection(movement);
-  console.log("== movement:", movement);
 }
 
-function filterSelection(c) {
-
-  var x, i;
-  x = document.getElementsByClassName("workout");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    removeWorkout(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) addWorkout(x[i], "show");
-  }
-}
-
-// Show filtered elements
-function addWorkout(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+function filterSelection(movement) {
+  var workouts = document.getElementsByClassName("workout");
+  for (var i = 0; i < workouts.length; i++) {
+    var muscle = workouts[i].getAttribute('data-movement');
+    if(muscle.includes(movement)) {
+      workouts[i].style.display = "";
+    } else {
+      workouts[i].style.display = "none";
     }
   }
-}
-
-// Hide elements that are not selected
-function removeWorkout(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
 }
 
 // Add active class to the current control button (highlight it)
@@ -73,8 +48,8 @@ var btnContainer = document.getElementById("filterBtn");
 var btns = btnContainer.getElementsByTagName("button");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
+    var active = document.getElementsByClassName("active");
+    active[0].className = active[0].className.replace(" active", "");
     this.className += " active";
   });
 }
